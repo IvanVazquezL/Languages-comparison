8b95d0e11bca72410c7bc7247627371a01922fcc
IvanVazquezL <ivanvl_11@hotmail.com>
2020-07-21 09:48:56 -0500

    Hello World

---
+++ helloWorld/helloWorld.cs
@@ -0,0 +1,18 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Hello World");
+  }
+}
+
+// using System.IO;
+// using System;
+//
+// class Program
+// {
+//     static void Main()
+//     {
+//         Console.WriteLine("Hello, World!");
+//     }
+// }
